300f6965f167b76c075aa5467d9601ff08759698
Shivam Marwaha <shivam.marwaha95@gmail.com>
2024-01-01 17:48:34 +0530

    udpated name

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,28 +1,30 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Mail, Twitter, Instagram } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
+	
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
-	},
-	{
-		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
-		label: "Email",
-		handle: "dev@chronark.com",
-	},
-	{
-		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		icon: <Instagram size={20} />,
+		href: "https://www.instagram.com/shivam.shoots/",
 		label: "Github",
 		handle: "chronark",
 	},
+	{
+		icon: <Twitter size={20} />,
+		href: "https://twitter.com/shivam_codes",
+		label: "Twitter",
+		handle: "@shivam.codes",
+	},
+	{
+		icon: <Mail size={20} />,
+		href: "mailto:info@shivam.shoots.xyz",
+		label: "Email",
+		handle: "dev@chronark.com",
+	},
+	
 ];
 
 export default function Example() {
